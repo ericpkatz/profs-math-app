a3f19a707bd8183750d424c3cc9a00f0a212a994
ericpkatz <ericpkatz@gmail.com>
2021-10-05 17:35:15 -0400

    commit

--- spec.js
+++ spec.js
@@ -1,9 +1,35 @@
 const { expect } = require('chai');
+const Sequelize = require('sequelize');
+console.log(process.env.DATABASE_URL);
+const conn = new Sequelize(process.env.DATABASE_URL);
+const { STRING } = Sequelize;
+const syncAndSeed = async()=> {
+  await conn.sync({ force: true });
+  await Promise.all([
+    User.create({ name: 'moe'}),
+    User.create({ name: 'lucy'}),
+    User.create({ name: 'ethyl'}),
+  ]);
+};
+
+const User = conn.define('user', ()=> {
+  name: STRING
+});
 
 describe('Math', ()=> {
   describe('1 + 1', ()=> {
     it('is 2', ()=> {
       expect(1 + 1).to.equal(2);
+    });
+  });
+});
+
+describe('My Database', ()=> {
+  beforeEach(()=> syncAndSeed());
+  describe('User.findAll', ()=> {
+    it('there are 3 users', async()=> {
+      const users = await User.findAll();
+      expect(users.length).to.equal(3);
     });
   });
 });
